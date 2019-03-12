660f797198a481ce8d3237ff7cbab088893c7b0c
kajakIYD <ficupaolo@gmail.com>
2019-03-12 02:39:04 +0100

    Remove Person, rename ServiceFeature to ServicesElement, add PortfolioElement

--- FlowerShop/flower_shop/models.py
+++ FlowerShop/flower_shop/models.py
@@ -3,14 +3,16 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):
-    name = models.CharField(max_length=130)
-    email = models.EmailField(blank=True)
-    job_title = models.CharField(max_length=30, blank=True)
-    bio = models.TextField(blank=True)
 
-
-class ServiceFeature(models.Model):
+class ServicesElement(models.Model):
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=300)
-    image = models.ImageField(upload_to='site_media')
+    image = models.CharField(max_length=200)
+    order_on_website = models.IntegerField()  #TODO: Make this field distinct
+
+
+class PortfolioElement(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=300)
+    image = models.CharField(max_length=200)
+

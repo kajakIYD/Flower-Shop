8dca3cd7732500a38125f564311b59ffed7fdae6
kajakIYD <ficupaolo@gmail.com>
2019-03-15 14:03:10 +0100

    Fix spacing in TODO comment

--- FlowerShop/flower_shop/models.py
+++ FlowerShop/flower_shop/models.py
@@ -8,7 +8,7 @@ class ServicesElement(models.Model):
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=300)
     image = models.CharField(max_length=200)
-    order_on_website = models.IntegerField()  #TODO: Make this field distinct
+    order_on_website = models.IntegerField()  # TODO: Make this field distinct
 
 
 class PortfolioElement(models.Model):
